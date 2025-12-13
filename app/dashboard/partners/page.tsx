d354a61b2cfaea4a12458426945f7b21d3f8bdb1
Doorplace Admin <admin@doorplaceusa.com>
2025-12-13 04:29:55 -0600

    layout

--- app/dashboard/partners/page.tsx
+++ app/dashboard/partners/page.tsx
@@ -2,9 +2,9 @@ export default function Page() {
   return (
     <div>
       <h1 className="text-2xl font-bold">[Partners]</h1>
-      <p className="mt-3 text-gray-600">TEST TEST TEST Hello TEST More features coming soon.</p>
+      <p className="mt-3 text-gray-600">Comming Soon Thanks.</p>
     </div>
   );
 }
 
-<h1>SERVER TEST 123</h1>
+

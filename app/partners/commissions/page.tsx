d38be39b750ef9670827635b9854e2ec2c0d6a6f
Doorplace Admin <admin@doorplaceusa.com>
2025-12-25 20:30:32 -0600

    upgrades

--- app/partners/commissions/page.tsx
+++ app/partners/commissions/page.tsx
@@ -1,5 +1,6 @@
-// app/partners/commissions/page.tsx
+
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useEffect, useMemo, useState } from "react";
 const [partnerId, setPartnerId] = useState<string | null>(null);
